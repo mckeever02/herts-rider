0120b4913a3978e5c30a870766317010899f7d5b
Michael <mckeever02@gmail.com>
2015-06-22 20:23:47 +0100

    content added for how to buy a motorbike article

---
+++ articles/how-to-buy-a-motorbike/index.php
@@ -0,0 +1,199 @@
+<!-- Set the Page ID -->
+<?php $page_id = 21; ?>
+
+<?php include ("../../includes/variables.php"); ?>
+<?php include ("$path/includes/doctype.php"); ?>
+
+  <meta name="author" content="FirstCar">
+
+  <?php include ("$path/includes/head.php"); ?>
+
+</head>
+
+<body id="<? echo $page[''.$page_id.'']['id'];?>" class="drawer drawer-right">
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <header class="page-header dark imgix-fluid-bg dark-overlay" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/page-header.jpg?auto=format,enhance&fit=crop" role="banner">
+        <?php include ("$path/includes/logo.php"); ?>
+        <?php include ("$path/includes/menu.php"); ?>
+
+        <div class="article-intro offset-by-six columns">
+            <h1>How to buy a motorbike</h1>
+            <p class="blurb">Buying a motorbike can be as daunting as it is exciting. Here’s how to make sure you don’t regret your decisions</p>
+        </div>
+
+        <?php include ("$path/includes/prev-next.php"); ?>
+    </header>
+
+    <article class="standard-article">
+
+        <section class="container wow fadeInUp">
+
+          <div class="longform-text-container">
+            <p class="introduction">Whether you’re looking at doing a deal on a brand new machine or snapping up a secondhand steed, there’s lots to consider if you’re to make the right choice when buying a motorbike. Get it wrong and it can be a costly hell, get it right and it’s heaven.</p>
+
+            <p>Over the following article we’ll steer you through the minefield that is buying a new or used motorbike &mdash; so you end up with the bike that’s right for you.</p>
+          </div><!-- /longform-text-container -->
+
+          <blockquote>
+            <span class="small-text footnote">Top tip</span>
+            <p>Remember to budget for insurance cover. Third party is the minimum with fully comprehensive the best, but most expensive option</p>
+          </blockquote>
+
+          <div class="longform-text-container">
+            <p>If you’re a new rider you’ll potentially face high premiums, but this will drop as your experience and no-claims bonus increases. Sporty bikes will also attract higher premiums, whereas scooters and commuter bikes are usually less.</p>
+
+            <p>Be sure to declare everything when applying so you’re covered in the event of a claim. To get the best deal, ring round or check online to find the most competitive deal.</p>
+
+            <p>Search for bike insurance specialists online to cover all the bases, and make sure you check out our <a href="../motorcycle-insurance/">page on getting the best insurance deal</a>.</p>
+
+          </div>
+        </section>
+
+        <section class="container numbered">
+          <h1>Buying a new motorbike</h1>
+
+          <div class="longform-text-container">
+            <p>Buying new from a dealer means you’ll know your bike hasn’t been abused and it’ll probably come with at least two years’ warranty. But you’ll pay for these benefits, as depreciation is greatest in the first year of ownership.</p>
+          </div>
+        </section>
+
+        <section class="container">
+
+            <h3 class="u-text-center">Where to buy</h3>
+
+            <div class="row">
+              <div class="one-half column list-number-box">
+                <div class="u-text-center">
+                  <span class="circle-container small">1</span>
+                  <h4 class="color">Franchised dealer</h4>
+                <p>These have an agreement with a manufacturer to sell their motorcycles. You should be able to get a decent test ride, and top quality service, but you’ll pay the highest price for the privilege. However, there is the option to finance.</p>
+                </div><!-- /u-text-center -->
+              </div><!-- /one-half column -->
+
+              <div class="one-half column list-number-box">
+                <div class="u-text-center">
+                  <span class="circle-container small">2</span>
+                  <h4 class="color">Independent dealer</h4>
+                <p>Some dealers specialise in certain makes or types of bike, while others have a huge stock of anything and everything. You should still expect good back up including warranties, finance and the option to part-exchange. Bikes are likely to be slightly cheaper than a franchised dealer, as are servicing costs.</p>
+                </div><!-- /u-text-center -->
+              </div><!-- /one-half column -->
+
+            </div><!-- /row -->
+        </section>
+
+        <section class="container">
+
+          <h3 class="u-text-center">Buying tips</h3>
+
+          <div class="row">
+
+            <div class="one-third column list-number-box">
+              <div class="u-text-center">
+                <span class="circle-container small">1</span>
+              <p>Bring both parts of your driving licence and ID if you want a test ride. Major retailers will have demonstrators, but other dealers may have a used model you can test. Try other bikes that fit your criteria too.</p>
+              </div><!-- /u-text-center -->
+            </div><!-- /one-third column -->
+
+            <div class="one-third column list-number-box">
+              <div class="u-text-center">
+                <span class="circle-container small">2</span>
+              <p>Haggling to get the price down is part of the process, so make a lower offer. Check you’re negotiating the On The Road (OTR) price, which includes all those little extra charges that can add up. Ask about what extras they can give you – luggage, a new helmet, clothing or security products. You could walk away with a bundle if the salesman is desperate for a sale.</p>
+              </div><!-- /u-text-center -->
+            </div><!-- /one-third column -->
+
+            <div class="one-third column list-number-box">
+              <div class="u-text-center">
+                <span class="circle-container small">3</span>
+              <p>Bring both parts of your driving licence and ID if you want a test ride. Major retailers will have demonstrators, but other dealers may have a used model you can test. Try other bikes that fit your criteria too.</p>
+              </div><!-- /u-text-center -->
+            </div><!-- /one-third column -->
+
+          </div><!-- /row -->
+
+        </section>
+
+        <section class="container">
+          <h3 class="u-text-center">Finance</h3>
+
+          <div class="longform-text-container">
+            <p>If you’ve not got the cash for a new bike, finance could be the way to go, spreading the cost of your bike into more manageable portions over a period of time that suits you. Many manufacturers offer great finance deals on new bikes – some even come with a year’s free insurance – which could save you a packet. Look online for the latest offers from all the key motorcycle manufacturers.</p>
+          </div>
+        </section>
+
+        <section class="container">
+          <h3 class="u-text-center">Back up</h3>
+
+          <div class="longform-text-container">
+            <p>Buying from a dealer means you’re protected by the Sale of Goods Act, which says goods must meet an acceptable standard. If your bike develops a fault the dealer must fix it; if there’s a major problem you can ask for a replacement or refund.</p>
+          </div>
+        </section>
+
+        <section class="container numbered">
+          <h1>Buying a used motorbike</h1>
+
+          <div class="longform-text-container">
+            <p>Buying used is a great way to get more for your money, but buying secondhand can also mean dodgy dealers, criminal cloners and unscrupulous private sellers. This essential checklist will help you buy with confidence.</p>
+          </div>
+        </section>
+
+        <section class="container">
+
+          <div class="row">
+            <div class="one-half column list-number-box">
+              <div class="u-text-center">
+                <span class="circle-container small">1</span>
+                <h4 class="color">Arm yourself with info</h4>
+              <p>Learn as much as you can about a bike before viewing it. Magazines and internet forums can give you information about common problems and what to look out for. Finding out the cost of consumables such as tyres and chains could give you more haggling power if these need replacing. Also, check what the going rate is for that model and age by comparing it with similar bikes for sale.</p>
+              </div><!-- /u-text-center -->
+            </div><!-- /one-half column -->
+
+            <div class="one-half column list-number-box">
+              <div class="u-text-center">
+                <span class="circle-container small">2</span>
+                <h4 class="color">Two’s company</h4>
+              <p>Take a mate along to inspect any potential purchase. It’s too easy to fall in love with a bike and see past its problems so two sets of eyes helps. </p>
+              </div><!-- /u-text-center -->
+            </div><!-- /one-half column -->
+
+          </div><!-- /row -->
+
+           <div class="row">
+            <div class="one-half column list-number-box">
+              <div class="u-text-center">
+                <span class="circle-container small">3</span>
+                <h4 class="color">Use your eyes</h4>
+              <p>Always view a secondhand bike in good weather in natural light to have the best chance of spotting any issues. Take your time to inspect every area of the bike. If anything looks wrong, it probably is. Be prepared to walk away – there are plenty of other used bikes out there, that you can buy.</p>
+              </div><!-- /u-text-center -->
+            </div><!-- /one-half column -->
+
+            <div class="one-half column list-number-box">
+              <div class="u-text-center">
+                <span class="circle-container small">4</span>
+                <h4 class="color">Fire her up</h4>
+              <p>Check the engine and exhaust for signs of warmth, which could mean the seller warmed the bike up before you got there, hiding starting problems. Once started, check for excessive engine noise or smoke. Check the operation of all lights, gauges, gears, brakes, horn and push on the bars and seat to see if the suspension settles properly.</p>
+              </div><!-- /u-text-center -->
+            </div><!-- /one-half column -->
+
+          </div><!-- /row -->
+
+        </section>
+
+
+
+
+
+        <?php include ("$path/includes/article-social-share.php"); ?>
+
+    </article>
+
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+   <?php include ("$path/includes/scripts.php"); ?>
+
+</body>
+</html>
---
+++ articles/routes-to-riding/index.php
@@ -0,0 +1,256 @@
+<!-- Set the Page ID -->
+<?php $page_id = 21; ?>
+
+<?php include ("../../includes/variables.php"); ?>
+<?php include ("$path/includes/doctype.php"); ?>
+
+  <meta name="author" content="FirstCar">
+
+  <?php include ("$path/includes/head.php"); ?>
+
+</head>
+
+<body id="<? echo $page[''.$page_id.'']['id'];?>" class="drawer drawer-right">
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <header class="page-header dark imgix-fluid-bg dark-overlay" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/page-header.jpg?auto=format,enhance&fit=crop" role="banner">
+        <?php include ("$path/includes/logo.php"); ?>
+        <?php include ("$path/includes/menu.php"); ?>
+
+        <div class="article-intro offset-by-six columns">
+            <h1>Routes to riding</h1>
+            <p class="blurb">Confused about what it takes to get onto two wheels? Here, all is explained...</p>
+        </div>
+
+        <?php include ("$path/includes/prev-next.php"); ?>
+    </header>
+
+    <article class="standard-article">
+
+        <section class="container wow fadeInUp">
+
+          <div class="longform-text-container">
+            <p class="introduction">All new riders must complete CBT (Compulsory Basic Training). It’s valid for two years; if you don’t take your full test after this time, you’ll have to redo your CBT.</p>
+          </div><!-- /longform-text-container -->
+        </section>
+
+        <section class="container">
+          <div class="row">
+            <div class="one-third column">
+              <h3>5 Counties Motorcycle Club</h3>
+              <ul>
+                <li>Based in Stevenage</li>
+                <li>Regular ride-outs and social events</li>
+                <li>Meets at various pubs on Monday nights from 7.30pm</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Contact:</strong> Clinton &amp; Colleen Spinas</li>
+                <li><strong>Telephone:</strong> <a href="tel:07507493705">07507 493705</a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>AJS &amp; Matchless Owners Club Ltd (Hertfordshire Section) </h3>
+              <ul>
+                <li>Group of British bike enthusiasts</li>
+                <li>Meets at the Goat, Hertford Heath on 1st Thursday of the month</li>
+                <li>Organised runs throughout year</li>
+                <li>Sell parts to non-members &mdash; details on website</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Web:</strong> <a href="http://jampot.com">jampot.com</a></li>
+                <li><strong>Contact:</strong> Rob Harknett</li>
+                <li><strong>Telephone:</strong> <a href="tel:01279792329">01279 792329</a></li>
+                <li><strong>Email:</strong> <a href="mailto:prewar@jampot.com">prewar@jampot.com</a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>British Owners’ Club </h3>
+              <ul>
+                <li>Restoration and preservation of British motorcycles</li>
+                <li>Regular meetings and shows</li>
+                <li>Meets every Wednesday from 7.30pm at The Woodbine Inn, Honey Lane, Waltham Abbey</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Web:</strong> <a href="http://www.britishownersclubessex.co.uk">britishownersclubessex.co.uk</a></li>
+                <li><strong>Contact:</strong> Rob Woodford</li>
+                <li><strong>Telephone:</strong> <a href="tel:07974451158">07974 451158</a></li>
+                <li><strong>Email:</strong> <a href="mailto:rob.woodford@honeywell.com">prob.woodford@honeywell.com</a></li>
+              </ul>
+            </div>
+          </div><!-- /row -->
+      </section>
+
+      <section class="container">
+          <div class="row">
+            <div class="one-third column">
+              <h3>The Busy Bee Motorcycle Club</h3>
+              <ul>
+                <li>Informal club with regular ride-outs</li>
+                <li>Meets at Broadlakes Lodge Social Club, London Colney every Thursday evening</li>
+                <li>Meets at various pubs on Monday nights from 7.30pm</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Contact:</strong> Laurie Nelville</li>
+                <li><strong>Telephone:</strong> <a href="tel:01442257781">01442 257781</a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>Eana MCC</h3>
+              <ul>
+                <li>Informal group of local motorcyclists with ride-outs, rallies and social meetings</li>
+                <li>Meets at The King &amp; Tinker, Enfield every Thursday from 8.30pm</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Web:</strong> <a href="http://www.eanamcc.com">eanamcc.com</a></li>
+                <li><strong>Contact:</strong> Becky or James</li>
+                <li><strong>Telephone:</strong> <a href="tel:07788477141">07788 477 141</a> or <a href="tel:07710171166">07710 171 166</a></li>
+                <li><strong>Email:</strong> <a href="mailto:eana_mcc@yahoo.co.uk">eana_mcc@yahoo.co.uk</a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>Herts &amp; Beds Advanced Motorcyclists</h3>
+              <ul>
+                <li>Get a skill for life by joining the Herts &amp; Beds Advanced Motorcyclists (lAM)</li>
+                <li>Pass your advanced motorcycle test, socialise, better your riding. monthly ride-outs. talks etc</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Web:</strong> <a href="http://hbam.org.uk">hbam.org.uk</a></li>
+                <li><strong>Contact:</strong> Rob Woodford</li>
+                <li><strong>Telephone:</strong> <a href="tel:08004588425 ">0800 458 8425 </a></li>
+              </ul>
+            </div>
+          </div><!-- /row -->
+      </section>
+
+      <section class="container">
+          <div class="row">
+            <div class="one-third column">
+              <h3>Laser Motorcycle Club </h3>
+              <ul>
+                <li>Bikers of all abilities are welcome from novice through to advanced and beyond, on any make or age of motorcycle</li>
+                <li>Meets 1st and 3rd Thursday of the month at The Green Dragon, London Colney</li>
+                <li>Ride-outs 3rd Sunday of the month</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Web:</strong> <a href="http://www.lasermc.co.uk">lasermc.co.uk</a></li>
+                <li><strong>Email:</strong> <a href="mailto:eana_mcc@yahoo.co.uk">lasermc@lasermc.co.uk</a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>North Herts Scooter Collective</h3>
+              <ul>
+                <li>Contact for information on the clubs and meetings</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Contact:</strong>Julie Bell</li>
+                <li><strong>Telephone:</strong> <a href="tel:07843423262">07843 423 262</a></li>
+                <li><strong>Email:</strong> <a href="mailto:julie@northhertssc.co.uk">julie@northhertssc.co.uk</a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>Redliners MCC </h3>
+              <ul>
+                <li>Informal group of local motorcyclists with regular ride-outs and social meetings</li>
+                <li>Meets at The Swan, Pimlico. Contact below for information on meeting</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Contact:</strong>Tanya</li>
+                <li><strong>Telephone:</strong> <a href="tel:07865079639 ">07865 079 639</a></li>
+              </ul>
+            </div>
+          </div><!-- /row -->
+      </section>
+
+      <section class="container">
+          <div class="row">
+            <div class="one-third column">
+              <h3>Royston &amp; District Motorcycle Club</h3>
+              <ul>
+                <li>Mixed group of local motorcyclists</li>
+                <li>Ride-cuts, social meetings and charity fundraising events</li>
+                <li>Meets 1st and 3rd Wednesdays of the month at Eternit Sports &amp; Social Club. Nr Royston</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Contact:</strong>Peter Howes</li>
+                <li><strong>Telephone:</strong> <a href="tel:01763246865">01763 246 865</a></li>
+                <li><strong>Email:</strong> <a href="mailto:peter.howes@btltinternet.com">peter.howes@btltinternet.com</a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>Stevenage &amp; District Motorcycle Club</h3>
+              <ul>
+                <li>For all ages with regular ride-outs and social meetings</li>
+                <li>Based at the Robin Hood, Walkern</li>
+                <li>Meets at various pubs every Monday night throughout the year</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Contact:</strong>Mick Taylor</li>
+                <li><strong>Telephone:</strong> <a href="tel:01438354130 ">01438 354130 </a></li>
+              </ul>
+            </div>
+
+            <div class="one-third column">
+              <h3>Vintage Motorcycle Club, Chiltern Section</h3>
+              <ul>
+                <li>Talks, runs and other events mainly for older machinery</li>
+                <li>Meets 1st Wednesday of the month at the White Hill Centre, Chesham</li>
+              </ul>
+
+              <ul class="plain-list">
+                <li><strong>Contact:</strong>Mike Vangucci</li>
+                <li><strong>Telephone:</strong> <a href="tel:01494786706">01494 786 706</a></li>
+              </ul>
+            </div>
+          </div><!-- /row -->
+      </section>
+
+      <section class="container">
+        <div class="row">
+          <div class="one-third column">
+            <h3>Vintage Motorcycle Club, East Herts Section</h3>
+            <ul>
+              <li>Talks, runs and other events mainly for older machinery</li>
+              <li>Meets 3rd Tuesday of the month at Broadlakes Lodge Social Club, London Colney</li>
+            </ul>
+
+            <ul class="plain-list">
+              <li><strong>Contact:</strong>Dave Francis</li>
+              <li><strong>Telephone:</strong> <a href="tel:01992632372">01992 632372</a></li>
+            </ul>
+          </div>
+        </div><!-- /row -->
+      </section>
+
+
+        <?php include ("$path/includes/article-social-share.php"); ?>
+
+    </article>
+
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+   <?php include ("$path/includes/scripts.php"); ?>
+
+</body>
+</html>
