fa3a490a7d73fe6cabe900524c09fc499d38ccc9
Michael <mckeever02@gmail.com>
2015-06-24 11:22:25 +0100

    Added content to competition article

---
+++ articles/competition/index.php
@@ -0,0 +1,168 @@
+<!-- Set the Page ID -->
+<?php $page_id = 21; ?>
+
+<?php include ("../../includes/variables.php"); ?>
+<?php include ("$path/includes/doctype.php"); ?>
+
+  <meta name="author" content="FirstCar">
+
+  <?php include ("$path/includes/head.php"); ?>
+
+</head>
+
+<body id="<? echo $page[''.$page_id.'']['id'];?>" class="drawer drawer-right">
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <header class="page-header dark imgix-fluid-bg dark-overlay" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/page-header.jpg?auto=format,enhance&fit=crop" role="banner">
+        <?php include ("$path/includes/logo.php"); ?>
+        <?php include ("$path/includes/menu.php"); ?>
+
+        <div class="article-intro offset-by-six columns">
+            <h1>Win over £750 worth of kit from <a href="http://www.sportsbikeshop.co.uk/"><img src="images/sportbikeshop-logo.png" alt="Sportsbikeshop.co.uk"></a></h1>
+
+        </div>
+
+        <?php include ("$path/includes/prev-next.php"); ?>
+    </header>
+
+    <article class="standard-article">
+
+        <section class="container wow fadeInUp">
+
+          <div class="longform-text-container">
+            <p class="introduction">Win the kit you need to stay safe on your bike, thanks to <a href="sportsbikeshop.co.uk">sportsbikeshop.co.uk</a>. You need the right kit when you’re on your motorbike, and thanks to SportsBikeShop you’ve got the chance to win over £750-worth of the best biking clobber.</p>
+
+            <p>You don’t just need safety kit such as a helmet, jacket, boots, gloves and trousers, you also need security and protection. That’s why SportsBikeShop has included a chain lock worth £40 and a bike cover to keep your pride and joy sheltered from the elements. Just look at this lot...</p>
+
+
+          </div><!-- /longform-text-container -->
+
+        </section>
+
+        <section class="container">
+          <div class="row">
+            <div class="one-half column">
+              <h2>Kabuto Kamui helmet</h2>
+              <h3>Worth £199.99</h3>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/helmet.jpg" alt="Kabuto Kamui helmet"></div>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="row">
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/boots.jpg" alt="Falco Boots Oxegen"></div>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <h2>Falco Boots Oxegen</h2>
+              <h3>Worth £129.99</h3>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="row">
+            <div class="one-half column">
+              <h2>ARMR Ashido 2 jacket</h2>
+              <h3>Worth £99.99</h3>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/boots.jpg" alt="ARMR Ashido 2 jacket"></div>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="row">
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/roadskin-jeans.jpg" alt="Roadskin Paranoid jeans"></div>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <h2>Roadskin Paranoid jeans</h2>
+              <h3>Worth £119.99</h3>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="row">
+            <div class="one-half column">
+              <h2>Lampa Kiton chain lock</h2>
+              <h3>Worth £39.99</h3>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/chain-lock.jpg" alt="Lampa Kiton chain lock"></div>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="row">
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/roadskin-jeans.jpg" alt="Lampa Optima bike cover"></div>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <h2>Lampa Optima bike cover</h2>
+              <h3>Worth £39.99</h3>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="row">
+            <div class="one-half column">
+              <h2>ARMR S550 gloves</h2>
+              <h3>Worth £49.99</h3>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/gloves.jpg" alt="ARMR S550 gloves"></div>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="row">
+            <div class="one-half column">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="https://firstcar.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/indo-trousers.jpg" alt="ARMR Indo trousers"></div>
+            </div><!-- /one-half column -->
+
+            <div class="one-half column">
+              <h2>ARMR Indo trousers</h2>
+              <h3>Worth £79.99</h3>
+            </div><!-- /one-half column -->
+          </div><!-- /row -->
+
+          <div class="u-center-table">
+            <a href="" class="button button-primary button-large animated pulse infinite">Enter for your chance to win</a>
+          </div><!-- /u-text-center -->
+
+
+
+        </section>
+
+        <section class="container">
+          <div class="longform-text-container">
+            <h4>The T's &amp; C's</h4>
+            <ul>
+              <li>This competition is run by FirstCar Limited and SportsBikeShop Limited.</li>
+              <li>Entries must be recieved by midnight on 1 June 2016 after which the winner will be randomly selected</li>
+              <li>For full terms and conditions see <a href="http://firstbikemagazine.co.uk">www.firstbikemagazine.co.uk</a></li>
+            </ul>
+          </div>
+
+        </section>
+
+
+
+
+
+        <?php include ("$path/includes/article-social-share.php"); ?>
+
+    </article>
+
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+   <?php include ("$path/includes/scripts.php"); ?>
+
+</body>
+</html>
