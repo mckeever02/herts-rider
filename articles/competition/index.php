1c55ce8e145aee1e3724a03c63482178e1cd8b51
Michael <mckeever02@gmail.com>
2015-06-29 11:56:52 +0100

    thumbnail fix

--- articles/competition/index.php
+++ articles/competition/index.php
@@ -112,9 +112,9 @@
 
 
           <div class="u-center-table">
-            <h2>Enter for your chance to win</h2>
+            <h2 class="u-text-center">Enter for your chance to win</h2>
 
-            <script>(function(t,e,c,s){var n,o,a;t.SMCX=t.SMCX||[],e.getElementById(s)||(n=e.getElementsByTagName(c),o=n[n.length-1],a=e.createElement(c),a.type="text/javascript",a.async=!0,a.id=s,a.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/SqaxiL9F5IL_2BaxSjltdpts7xBcz_2F81ztj34ctkSWg8M_2B39zZv0yh6FLfPUGKb_2B8u.js"].join(""),o.parentNode.insertBefore(a,o))})(window,document,"script","smcx-sdk");</script><a style="font: 12px Helvetica, sans-serif; color: #999; text-decoration: none;" href=https://www.surveymonkey.com/mp/customer-satisfaction-surveys/> Create your own user feedback survey </a>
+            <script>(function(t,e,c,s){var n,o,a;t.SMCX=t.SMCX||[],e.getElementById(s)||(n=e.getElementsByTagName(c),o=n[n.length-1],a=e.createElement(c),a.type="text/javascript",a.async=!0,a.id=s,a.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/SqaxiL9F5IL_2BaxSjltdpts7xBcz_2F81ztj34ctkSWg8M_2B39zZv0yh6FLfPUGKb_2B8u.js"].join(""),o.parentNode.insertBefore(a,o))})(window,document,"script","smcx-sdk");</script>
           </div><!-- /u-text-center -->
 
         </section>
