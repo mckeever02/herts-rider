0376aec8f4f529b5dabb02d2480d2842e80340b3
Tim Potter <tim@littlethunder.co>
2015-07-02 12:14:56 +0100

    Added Subscribe to Contents Page

--- articles/contents/index.php
+++ articles/contents/index.php
@@ -22,20 +22,42 @@
 
 
 
-      <ul class="contents">
+        <ul class="contents">
+            <li class="intro article-tile imgix-fluid-bg" data-src="https://herts-rider.imgix.net/articles/about/images/page-header.jpg?auto=format,enhance&fit=crop">
+                <a class="tile-link" href="../about">
+                    <h2>Welcome to Hertfordshire Rider</h2>
+                    <p class="opening">Charlie Boorman intro goes here.</p>
+                </a>
+                <a href="../about" class="button button-small button-icon">Find out more</a>
+            </li>
+            
+            <li class="intro article-tile imgix-fluid-bg" data-src="https://herts-rider.imgix.net/articles/contents/images/page-header.jpg?auto=format,enhance&fit=crop">
+               <a class="tile-link">
+                <h3>Subscribe to our Newsletter</h3>
+                <p>Keep up-to-date with Herts Rider by signing up to our Newsletter.</p>
+                
+                <?=$success?>
+                <form id="contact-form" name="contact-form" method="post" action="../../submit.php#form-container">
+                    <input type="text" placeholder="Enter your email" class="validate[required,custom[email]]" name="email" id="email" value="<?=(isset($_SESSION['post']['email']) ? $_SESSION['post']['email'] : '');?>" />
+                    <input type="submit" name="button" value="Subscribe" />
+                    <?=$str?>
+                </form>
+           
+                </a>
+            </li>
 
             <?php foreach($page as $item) {
-                echo "
-                    <li class=\"article-tile imgix-fluid-bg {$item['id']} {$item['type']}\" data-src=\"https://herts-rider.imgix.net/articles/{$item['id']}/images/page-header.jpg?auto=format,enhance&fit=crop\">
-                        <a class=\"tile-link\" href=\"../{$item['id']}\">
-                            <h3>{$item['title']}</h3>
-                            <p>{$item['blurb']}</p>
-                        </a>
-                        <a href=\"../{$item['id']}\" class=\"button button-small button-icon\">{$item['button']}</a>
-                    </li>
-                    ";
-                } ?>
-          </ul>
+            echo "
+                <li class=\"article-tile imgix-fluid-bg {$item['id']} {$item['type']}\" data-src=\"https://herts-rider.imgix.net/articles/{$item['id']}/images/page-header.jpg?auto=format,enhance&fit=crop\">
+                    <a class=\"tile-link\" href=\"../{$item['id']}\">
+                        <h3>{$item['title']}</h3>
+                        <p>{$item['blurb']}</p>
+                    </a>
+                    <a href=\"../{$item['id']}\" class=\"button button-small button-icon\">{$item['button']}</a>
+                </li>
+                ";
+            } ?>
+        </ul>
 
 
 
