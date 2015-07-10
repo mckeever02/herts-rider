0bbe7131f4d2593cd44ece288289ae1e980ae00d
Tim Potter <tim@littlethunder.co>
2015-07-10 10:45:52 +0100

    Imgix padding amend

--- articles/ten-of-the-best/index.php
+++ articles/ten-of-the-best/index.php
@@ -41,7 +41,7 @@
           <h1>S-Doc 100 Dry Chain Lube – £14</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/1.jpg" alt="S-Doc 100 Dry Chain Lube"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/1.jpg" alt="S-Doc 100 Dry Chain Lube"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>This chain lube is completely transparent and highly fling-resistant, developed in a no doubt high-tech German laboratory to a secretive formula. Mysterious... Dry lube is nothing new, but die-hards still complain that lube must be wet to lubricate. This just isn't the case. Despite being a solid, dry lubes can still reduce friction between two surfaces sliding against each other. Different layers of solid lube, bonded weakly at the molecular level, move against each other smoothly, creating the much needed lubrication we all need on our bike chains. The S-Doc lube won't pick up as much grit and dirt as a traditional gooey substance, and won't fling as much crud around either.</p>
@@ -55,7 +55,7 @@
           <h1>Knox Dry Inside Base Layer – From £55</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/2.jpg" alt="Knox Dry Inside Base Layer"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/2.jpg" alt="Knox Dry Inside Base Layer"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>This is a new Merino wool base layer from safety specialists Knox. It's got all the properties of Merino wool – moisture wicking, temperature regulating, breathable, and antibacterial – but twinned with cooling and quick drying synthetic fibres. The wicking process pulls sweat away from skin, making the wearer feel warmer and more comfortable. The best fibres for wicking away moisture are wool and synthetics such as polyester. Merino wool is a good choice due to its softer, less itchy feel and having anti-bacterial agents that reduce odours. The result is a quality thermoregulating undergarment that's British made – in the Lake District if you want to know – using Merino woven in Australia.</p>
@@ -69,7 +69,7 @@
           <h1>OptiMate Monitor – Starting at £19.99</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/3.jpg" alt="OptiMate Monitor"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/3.jpg" alt="OptiMate Monitor"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>The ladies and gents at OptiMate have designed a new dual function monitor, which displays the battery's state of charge and will also provide active charging monitoring, giving owners an immediate overview of the motorcycle’s electrical system. Driven by a micro-processor, the compact unit has 4 LEDs, showing whether the battery is out of charge, weak, acceptable or good. Unfortunately there aren't enough green ticks to tell your your battery is excellent. When the bike is running, owners can see at a glance whether its electrical system is operating properly. You don't need to check the health of the Optimate, as it comes with a three year warranty. Two versions are available to suit lead-acid batteries, and another for lithium batteries.</p>
@@ -83,7 +83,7 @@
           <h1>AM Custom Leather Race Suit – From £877</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/4.jpg" alt="AM Custom Leather Race Suit"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/4.jpg" alt="AM Custom Leather Race Suit"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>A set of custom leathers is the tailored suit of motorcycling. And just like a tailored suit, measurements are all-important to get a perfect fit. Expect to be asked to AM's headquarters near Durham for a measuring session. They'll take exact readings from over twenty different locations on your body, along with detailed photographs. AM suits are famous for using the fewest leather panels possible – that's around 23 different pieces. The fewer leather panels, the fewer stitches there are and the safer the suit will be. Customise your suit to your heart's content: add a racing hump for £35, a front hand vent for £15, or product badges for £2.50 each. Want to test your suit out for sidecar racing? AM have one casually loitering on site.</p>
@@ -97,7 +97,7 @@
           <h1>Shark Evoline ST Series 3 – £270</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/5.jpg" alt="Shark Evoline ST Series 3"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/5.jpg" alt="Shark Evoline ST Series 3"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>Want a full-face, flip up and open face helmet in one? You can flip the chin guard of the Evoline right back behind your head, allowing you to ride at speed without the sail effect produced by most flip-up helmets' chin guards. European Standard ECE 22.05/P tests helmets that have a protective lower face covering, and ECE 22.05/J is the code for no lower face protection. Very few flip-up helmets have passed both the P and J standards, but the Evoline has both. When trying it on, make sure it fits snugly around the ears – this is the secret to finding a quiet helmet.</p>
@@ -111,7 +111,7 @@
           <h1>Bull-it +7 VoloCE Jeans – From £190</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/6.jpg" alt="Bull-it +7 VoloCE Jeans"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/6.jpg" alt="Bull-it +7 VoloCE Jeans"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>These +7s offer the highest levels of protection found in motorcycle jeans at the moment. They are Level 2 CE approved, and sit right in the top price bracket for this type of gear. They come with CE armour at the hips and knees, and have a full length Covec +7 and breathable mesh lining. Covec technology uses uniquely engineered synthetic fibres that are knitted to offer high heat and abrasion resistance. The knitting process used by Covec offers a better drape than aramid fibre alternatives and feels softer to the skin when worn. The Covec +7 material has a lower thermal conductivity compared to the two other types of Covec liners on the market, but make a pair of jeans 0.5kg heavier.</p>
@@ -125,7 +125,7 @@
           <h1>Forcefield Pro Pants – £160</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/7.jpg" alt="Forcefield Pro Pants"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/7.jpg" alt="Forcefield Pro Pants"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>Forcefield has brought armour and base layers together into one tight-fitting package. Level 1 armour protects everywhere you'd need it – including your coccyx – all bound up with a technical fabric that wicks away moisture. Any armour that's loose is liable to move, and in the event of an accident it may displace from where it should be. If you can slide your armour to either side of your knee when donning your kit, then it's safe to say that your protection is too loose. Wearing under-armour, locked in place to your body, may be the best way to protect yourself when wearing jeans or textiles. These over trousers provide weather proofing and abrasion resistance, and the Pro Pants deal with impact protection.</p>
@@ -139,7 +139,7 @@
           <h1>Kriega US 30 Tail Pack – £95</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/8.jpg" alt="Kriega US 30 Tail Pack"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/8.jpg" alt="Kriega US 30 Tail Pack"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>This tail pack will keep your luggage completely secure when riding fast. It's a modular system that lets you attach extra packs straight to your US 30, easily bolting on 20 extra kilos to each side pannier-style. Fiddle around under your pillion seat to install textile straps to your bike's subframe and then hook the pack directly to those. The pack can be unclipped from these straps after a ride and you can walk away leaving the straps in place for easy pack re-attachment. The Kriega is claimed to be 100% waterproof and has an impressive ten year guarantee.</p>
@@ -153,7 +153,7 @@
           <h1>Tile Key Locator – £20 including shipping</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/9.jpg" alt="Tile Key Locator"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/9.jpg" alt="Tile Key Locator"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>Attach the nifty little Tile to your bike keys and download the Tile app to your smartphone. Then you can get a fix on them whenever you need to. The phone app tracks your keys via Bluetooth, not GPS, so the location isn't available over long distances. However, the app will record the last known location of your keys, and your phone will reconnect with the tile when you come back into range. Battery life of the Tile clocks in at a year, but isn't replaceable by users, so you'll need to buy a new one each year. You can run separate tiles from the same smartphone, and attach them to anything you need to locate.</p>
@@ -167,7 +167,7 @@
           <h1>TCX X-Blend Waterproof Boots – £150</h1>
           <div class="row">
             <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/10.jpg" alt="TCX X-Blend Waterproof Boots"></div>
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="https://herts-rider.imgix.net/articles/<? echo $page[''.$page_id.'']['id'];?>/images/10.jpg" alt="TCX X-Blend Waterproof Boots"></div>
             </div><!-- /one-half column -->
             <div class="one-half column">
             <p>The X-Blend says “vintage” as well as any other boot on the market, and it might fit the bill if you're looking for something casual to wear for urban riding. The rubber sole might not give you as much feel for the gear lever as a race boot, but it won't be missed if you're focussing on casual trips on a retro or café racer. Forget Velcro, laces are back in – black ones for the black boot, and brown for the tan leather option. Reports from recent wearers support TCX's claim that the boots are 100% waterproof, all the way to the top of the high collar.</p>
