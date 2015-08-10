b51683dffa44beb1e720638bb3da1b6a82e9082c
Tim Potter <tim@littlethunder.co>
2015-08-10 11:38:08 +0100

    Adding Logo

--- articles/about/index.php
+++ articles/about/index.php
@@ -75,11 +75,16 @@
         <section class="container lined wow fadeInUp">
             <div class="longform-text-container">
                 <h1>About Hertfordshire Rider</h1>
-                <p>This digital magazine is funded by Hertfordshire Council’s as a free source of information and advice for motorcyclists in around the county. Formerly titled Hertfordshire Biker and printed biannually, the magazine has evolved into a full digital publication with interactive content.</p>
- 
-<p>We hope you’ll find it much easier to access and share articles, which you can now view on your computer, phone or tablet. If you haven’t done so already, subscribe for free by clicking the button at the top of the homepage and letting us know your email address and we'll notify you when new issues come out. Don't forget to share the magazine with your friends using email or social media; just point them to <a href="http://hertfordshirerider.co.uk" target="_blank">hertfordshirerider.co.uk</a></p>
+                <div class="four columns reset-margin-left"><img src="../../images/herts-logo.jpg" alt="Hertfordshire Council"></div>
+                <div class="eight columns"><p class="introduction large-text">This digital magazine is funded by Hertfordshire Council’s as a free source of information and advice for motorcyclists in around the county. Formerly titled Hertfordshire Biker and printed biannually, the magazine has evolved into a full digital publication with interactive content.</p></div>
+                
+                <div class="group">
+                <p>We hope you’ll find it much easier to access and share articles, which you can now view on your computer, phone or tablet. If you haven’t done so already, subscribe for free by clicking the button at the top of the homepage and letting us know your email address and we'll notify you when new issues come out. Don't forget to share the magazine with your friends using email or social media; just point them to <a href="http://hertfordshirerider.co.uk" target="_blank">hertfordshirerider.co.uk</a></p>
 
             <p>Because Hertfordshire Rider is produced for you, tell us what you'd like to see in future editions by dropping us a line at any time on <a href="mailto:info@firstcar.co.uk">info@firstcar.co.uk</a> or call the office on <a href="tel:08451308853">08451 308 853</a>.</p>
+            </div>
+ 
+
                 
             </div>
         </section>
