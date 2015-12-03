589588a03f08aba3fe24f09737bb15b2e2a7edc5
Michael Mckeever <mckeever02@gmail.com>
2015-12-03 20:29:50 +0000

    Content added for Zen and the art of motorcycle riding

--- articles/art-of-motorcycle-riding/index.php
+++ articles/art-of-motorcycle-riding/index.php
@@ -1,5 +1,5 @@
 <!-- Set the Page ID -->
-<?php $page_id = 12; ?>
+<?php $page_id = 5; ?>
 
 <?php include ("../../includes/variables.php"); ?>
 <?php include ("$path/includes/doctype.php"); ?>
@@ -29,74 +29,57 @@
 
     <article class="standard-article">
 
-        <section class="container wow fadeInUp">
+        <section class="container">
 
           <div class="longform-text-container">
-            <p class="introduction">Keep your bike insured and you’re legal; fail to, and the cops will catch up with you sooner or later. When they do, if you can’t prove you’re insured, your bike can be seized and you’ll get 6-8 points on your licence.</p>
+            <p class="introduction">Whether you’re a new rider or a grizzly seasoned dispatcher, one key quality all riders need is self-awareness. I don’t mean you need to take an existential journey into the meaning of existence and your place within it, but you do need to have some idea of how you react to difference circumstances and situations.</p>
 
-            <p class="u-cf">When you’re taking out your cover, make sure you’re completely honest &mdash; lying to cut the premiums is a seriously bad idea. When Craig bought a new bike with a custom paint job and race exhaust he decided to tell his insurer that it was completely standard. He crashed a few weeks later crashed but his insurer refused to pay up, because the assessor could see the bike had been modified. And as Craig had chosen to lie, the insurer cancelled the policy. Remember, insurance cover is a two-way contract between you and your insurer. You wouldn’t expect them to lie to you &mdash; so don’t lie to them.</p>
+            <p>As a biker you have to process huge amounts of information fairly quickly in order to assess risk and make sensible decisions. Part of how you do this is by assessing other road users based on their previous behaviour and probably, rightly or wrongly, what they’re driving/riding or how they’re dressed.</p> 
 
           </div><!-- /longform-text-container -->
-        </section>
-
-        <section class="container wow fadeInUp">
-          <div class="row">
-            <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:115%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/right-kit.jpg" alt="Wear the right kit"></div>
-            </div><!-- one-half column -->
-
-            <div class="one-half column">
-              <h2>Wear the right kit</h2>
-              <p>Aside from the safety benefits of wearing proper protective equipment if you have an accident, it could also help you financially. That’s because if you’re not wearing suitable protective equipment when you’re in a collision, a personal injury award can be reduced by anywhere between 10% and 25%, so as well as being injured, you may also be out of pocket.</p>
-            </div><!-- /one-half column -->
-          </div><!-- /row -->
-
-          <div class="longform-text-container">
-            <h2>Excesses</h2>
-            <p>The excess on your policy is the contribution you must make in the event of a claim. It can typically be set at anywhere between £100 and £500, and it’ll be set at the point that you take out your cover. This will have an impact on your premium – a higher excess usually equates to a lower premium and vice versa, but make sure that you can afford to pay your excess in the event of a claim.</p>
-
-            <h2>No-claims discount</h2>
-            <p>Once you’ve got your policy sorted, for every year you insure a bike without making a claim, you’ll earn a year’s no-claims bonus (NCB), otherwise known as a no-claims discount (NCD), usually up to a maximum of five years. How much this is worth depends on your insurer, but it’s not unusual for a five-year NCD to cut your premium by up to 60%. If you have a crash, do the maths and make sure it’s worth claiming for, as you’ll lose your No Claims Discount if you do, and could end up with much higher premiums for years to come. </p>
-          </div><!-- /text-container -->
-        </section>
-
-        <section class="container">
-          <a href="http://www.carolenash.com/rider-cover" target="_blank"><div class="imgix-fluid-bg imgix-image" style="padding-bottom:33%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/carole-nash-banner.jpg" alt="Carole Nash"></div>
-          </a>
-        </section>
-
-        <section class="container">
-          <div class="row">
-
-            <div class="one-half column">
-              <h2>Biking saves you cash</h2>
-              <p>You can save a bundle of cash insuring a bike or scooter over a car - despite a bike being seen as more vulnerable. Figures from the Department for Transport show there are 10% fewer 17-year olds with a full driving licence than a decade ago, because insurance can cost around £2000 on average for a car at that age.</p>
-
-              <p>All the major motorcycle insurers such as Carole Nash, Lexham and Bikesure have seen more youngsters stay with two wheels for longer. Robert Balls of Bikesure says: &ldquo;A 17-year-old can insure a 125cc bike for about £400 a year. That means you’re looking at a quarter of the average premium of a car and some car premiums - depending on postcode can be ten times as much as a two-wheel premium. Add in the savings that can be made in fuel, road tax and parking and you can see that for some teenagers the only cost-effective transport they can buy is two-wheeled.&rdquo;</p>
-
-              <p>The cost of your premium in the future is in your  hands. Take advanced courses, don’t get points on your licence, consider a limited mileage policy, fit security and join a bike club. These things all help reduce your premium.</p>
-
-              <a href="http://www.carolenash.com/rider-cover" target="_blank"><div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/carole-nash-square.jpg" alt="Carole Nash"></div></a>
-
-            </div><!-- /one-half column -->
-
-            <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/scooter.jpg" alt="Man on scooter"></div>
-              <blockquote><p>&ldquo;A 17-year-old can insure a 125cc bike for about £400 a year.&rdquo;</p></blockquote>
-            </div><!-- one-half column -->
-
-          </div><!-- /row -->
-        </section>
-
-        <?php include ("$path/includes/article-social-share.php"); ?>
-
-    </article>
 
 
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+            <div class="row">
+              <div class="two-thirds column offset-by-one-third">
+                <blockquote>
+                  <p>It’s easy to focus on the road, other road users and the conditions instead of yourself.</p>
+                </blockquote>
+              </div>
+            </div>
 
-   <?php include ("$path/includes/scripts.php"); ?>
+            <div class="longform-text-container">
+
+
+            <p>But what about yourself? How aware of your own behaviour in different situations are you, and how you react in different circumstances? When you start riding, or do only relatively short trips in good conditions, it’s easy to focus on the road, other road users and the conditions instead of yourself.</p> 
+
+            <p>But for those who are riding all year or undertaking longer trips it’s important to know how being tired, or hungry, or scared or thrilled affect your decision making and therefore your riding. Because if you’re making decisions differently from usual you’re probably at more or less risk than usual too. And that’s when things can go wrong. Potentially very wrong.</p>
+
+            <h2>Fatigue</h2>
+            <p>Being tired is the most common one, especially at this time of year or if you’re off touring and doing long days in the saddle. Be aware if you slow down, or speed up, or get more frustrated or angry when you’re tired. Think about how you’re reacting and make allowances in your riding plan for how you’re thinking and for the fact the whole decision making process could take longer.</p>
+
+            <p>Tiredness can also cause your vision to drop so you see things later, which is why you might need to slow, to compensate. Alternatively, you could consciously raise your vision regularly, perhaps in conjunction with slowing.</p>
+
+            <h2>Hungry/thirsty/need a smoke</h2>
+
+            <p>It’s easy to ride through lunch, or not stop for water. Learn how your body displays dehydration or hunger symptoms, or that nicotine urge, and recognise the signs so you can stop and deal with it. Or if stopping really isn’t an option work out how you can deal with the changed psychical and mental state caused by these.</p>
+
+            <h2>Scared/thrilled/angry</h2>
+
+            <p>All sorts of situations occur to change your mood in an instant. How do you react? Get to know if you’re likely to chase someone down and kick their door – and perhaps work on a bit of anger management. Does having an awesome ride on new roads in stunning scenery make you more or less risk-averse?</p> 
+
+            <p>The more often you take the time to think ‘why is x, y or z happening?’ the better your decision making will be, the more you’ll enjoy your riding and the safer you’ll be.</p>
+
+          </div><!-- /longform-text-container -->
+
+          <?php include ("$path/includes/article-social-share.php"); ?>
+
+      </article>
+
+
+  <!-- End Document
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+     <?php include ("$path/includes/scripts.php"); ?>
 
 </body>
 </html>
