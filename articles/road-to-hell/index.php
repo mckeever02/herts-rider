05f37280c3e5d172d287f25d980724da516bc854
Michael Mckeever <mckeever02@gmail.com>
2015-12-03 21:26:00 +0000

    Content added for road to Hell article

--- articles/road-to-hell/index.php
+++ articles/road-to-hell/index.php
@@ -29,115 +29,58 @@
 
     <article class="standard-article">
 
-        <section class="container wow fadeInUp">
-
-          <div class="longform-text-container">
-
-            <p class="introduction">Hopefully you’ll never be the first on the scene of a road crash, but if you were, would you know what to do? What if you were out riding with your mates and one of them was involved in a collision &mdash; but you didn’t know how to save their life?</p>
-
-            <p>It’s easy to think that this won’t happen to you, but sadly it’s a scenario that’s far from unusual. That’s why the Biker Down programme was created &mdash; to help bikers help each other, and also to prevent crashes happening in the first place.</p>
-
-            <p>Bearing in mind doing the Biker Down course can save your life or the lives of your friends, it’s pretty good value &mdash; because it’s free. Even better, it’s not as though you have to make a huge time commitment because this three-module course takes just three hours to complete. The three topics covered are:</p>
-
-
-          </div><!-- /longform-text-container -->
-        </section>
-
-        <section class="container numbered wow fadeInUp">
-
-          <h1>What to do when first on a crash scene</h1>
-          <div class="row">
-            <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/crash-scene.jpg" alt="Crash scene example"></div>
-            </div>
-            <div class="one-half column">
-              <p>A Fire Bike team member guides you through how to deal with a crash scene involving a motorcyclist, including how you can ensure your own safety. You’ll find out what information you need to gather, to help the emergency services as well as any legal obligations.</p>
-            </div>
-          </div>
-        </section>
-
-        <section class="container numbered wow fadeIn">
-
-          <h1>Basic lifesaving first aid</h1>
-
-          <div class="row">
-            <div class="one-half column">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/firstaid-course.jpg" alt="Lifesaving first aid course"></div>
-            </div><!-- /one-half column -->
-
-            <div class="one-half column">
-              <p>Delivered by Herts Fire &amp; Rescue Service instructors, this module covers:</p>
-
-              <ul class="medium-list">
-                <li>Trauma and mechanism of injury</li>
-                <li>Snatch rescues, why when and how</li>
-                <li>Basic  life support</li>
-                <li>How to give CPR</li>
-                <li>Managing spinal injuries</li>
-                <li>When and how to safely remove a crash helmet</li>
-              </ul>
-            </div><!-- /one-half column -->
-
-          </div><!-- /row -->
-
-        </section>
-
-        <section class="container numbered wow fadeIn">
-
-          <h1>How to be more visible and prevent collisions at junctions</h1>
-
-          <div class="longform-text-container">
-
-          <p>Not being seen by other road users is one of the main reasons why motorcyclists are so often involved in collisions. Here you can find out how you can make yourself more visible without having to resort to covering yourself from head to foot in luminous yellow. Designed by advanced instructor Kevin Williams of Survival Skills Motorcycle Training, this module will prove to be an eye opener &mdash; guaranteed.</p>
-
-          </div>
-
-        </section>
-
         <section class="container">
+
           <div class="longform-text-container">
+            <div class="row">
+              <div class="one-third column">
+                <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/rob-box.jpg" alt="Rob Box"></div>
+              </div><!-- one-third column -->
+              <div class="two-thirds column">
+                <p class="introduction">What do you think is the best way to commute? How do you approach your daily commute? What are your mindset and goals? What do you think about before you set off?</p>
+                <p>You’ve probably never thought consciously about any of these things, yet all of them can make a real difference to the way you ride, and particularly how much you enjoy your riding. If you’re just a leisure rider this is important – but it’s even more so if you commute by bike.</p>
 
-            <h2>Get involved</h2>
+              </div>
+            </div><!-- /row -->
 
-            <p>You can take part in the Biker Down course if you’re part of a large biking community, a club or an organisation – or as an individual. If you’re booking on behalf of a club you’ll need to accurately predict how many people can guarantee their attendance and we’ll dedicate an event to that one group.
-            </p>
-            <p>If you want to take part as an individual just email us (details below) and you’ll be contacted as soon as an event becomes available. Once a booking is agreed please ensure you can honour it as the spaces on the courses are much sought after and non-attendance means someone else will miss out.
-            </p>
+            <p>When I switched from commuting by train into London, to doing it on a bike, I felt under pressure to prove I’d made the right choice. Pressure I’d created myself based on conversations I’d had with my mates. I wanted to show them I’d made the right call by making it quicker, easier and therefore better.</p>
 
-            <p><strong>Those who have taken the Biker Down course say:</strong></p>
 
-            <ul class="medium-list">
-              <li>Quality instruction and presentation from all instructors. Learnt new skills and knowledge from the start of the course and throughout. Fantastic idea, thank you very much.</li>
-              <li>Length of each element just long enough to maintain interest. Instructors particularly good at their specialist subjects and put it across in a good friendly manner. Will recommend to all my motorcycle friends.</li>
-              <li>Excellent. Instructive and thought provoking. Great to have quality input delivered by engaging people. An excellent introduction to biker first response. Presenter’s knowledge was first class with an added sense of humour.</li>
-              <li>A great course, really informative and interesting with a great group of instructors and presenters. I will be recommending it to everyone I know who rides.</li>
-            </ul>
-          </div>
+            <p>This made me ride a certain way, even on my Honda CG 125. My mindset was ‘faster than the train’, so I went fast. Because I wanted to go fast, anyone who wasn’t of the same mindset as me was in my way. I didn’t ask myself any questions when I got on the bike. I didn’t really care about anyone else using the roads because I was ‘fastest’.</p>
 
-          <blockquote>
-            <p>Find out more on the <a href="https://www.facebook.com/search/results/?init=quick&q=Biker%20Down%20Herts%20&tas=0.6477912096306682">Biker Down Herts Facebook page</a> Or email <a href="mailto:bikerdown@hertfordshire.gov.uk">bikerdown@hertfordshire.gov.uk</a></p>
-          </blockquote>
-        </section>
+            <div class="row">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/motorcyclists-in-london.jpg" alt="Motorcyclists in London"></div>
+              <blockquote>
+                <p>&ldquo;A guy in a black cab took care of the Fireblade for me, side swiping me at about 45mph while I was filtering traffic. The bike was a write off. Luckily, I was fine.&rdquo;</p>
+              </blockquote>
+            </div><!-- /row -->
 
-        <section class="container">
-          <div class="longform-text-container">
-            <h2>And there’s more...</h2>
-            <p>If you’re new to biking you’ll hopefully want to build on your existing skills. And if you’ve already enjoyed several years in the saddle, it could be time for a refresher course &mdash; especially as it’s free.</p>
+            <div class="container-margin-top">
 
-            <p>As part of the Biker Down programme you can have an assessed ride with a qualified advanced motorcyclist who will give you impartial, expert feedback on what you’re doing well and where you could develop your skills.</p>
+              <p>Plenty of arguments, accidents and bikes later I’m of a different mindset. I’m not riding a Honda CG 125 any more. I moved onto a Honda Fireblade about a year after I’d passed my test and whilst on this bike I used to think I was at the peak of my prowess. Little did I know...</p>
+              <p>I’d be knee down round the corners on my way home from work, Mark Marquez style. London to Hertford in 45 minutes. Wheelies and racing other guys on bikes away from traffic lights. Then a guy in a black cab took care of the Fireblade for me, side swiping me at about 45mph while I was filtering traffic. The bike was a write off. Luckily, I was fine.</p>
 
-            <p>Each assessed ride lasts around 90 minutes and covers about 20 miles. While the ride is not a training package, it will highlight any safety issues so you know if it’s worth booking yourself onto a training course with a third party.</p>
+              <p>At that point in my riding career I decided to make a change. I no longer wanted to ride a superbike to commute to work, so I bought a Honda Silverwing 600. A twist-and-go scooter with a 600cc engine, it’s the sort of thing you’d rib your granddad for riding. I didn’t care. I’d already decided I was switching my mindset as well as my bike.</p>
 
-            <p>Before booking onto a Biker Down course make it clear that you’d also like an assessed ride afterwards. The ride is free but if you’re given an IAM observer you may wish to make a contribution towards their petrol costs.</p>
+              <p>These days, before and while I’m riding, I focus on relaxing and taking my time. I watch out for other road users and I enjoy sharing the roads. Being kind and courteous to other road users while knowing the rules of the road makes life so much easier for everyone. I ride within my limits, don’t rush my journey (I’m only going to work after all), but most importantly, I can’t ride any faster than the traffic and traffic lights will allow me.</p>
+              <p>I’ve tested this theory too. If I try to ride home in a tearing hurry, opening it up between sets of traffic lights and increasing my speed where possible, it saves me perhaps 5-10 minutes. But how much difference does that really make to my life? Enough to make it worth the extra risk that comes with going that little bit faster? No chance. </p>
 
-            <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/fire-service.jpg" alt="Fire service"></div>
-
+              <p>I see guys on superbikes racing around the streets of London and they may make it through a couple of sets of traffic lights where I’ll be held up. But you can bet that somewhere down the road, at another set of traffic lights we’ll be there, at the front, together.</p>
+              <p>I’m far from unique in changing my mindset over many years of riding, but unlike a lot of motorcyclists I’ve lived to tell the tale. What a shame that many riders don’t see that by riding less competitively they’re much safer and so are those around them. And just as importantly, by relaxing more they’ll be a nicer, happier, less stressed person. Who wouldn’t want that?</p>
+            </div><!-- /container -->
           </div><!-- /longform-text-container -->
 
+          <div class="row">
+            <blockquote>
+              <p>&ldquo;What a shame that many riders don’t see that by riding less competitively they’re much safer and so are those around them.&rdquo;</p>
+            </blockquote>
+          </div>
 
         </section>
 
-        <?php include ("$path/includes/article-social-share.php"); ?>
+
+          <?php include ("$path/includes/article-social-share.php"); ?>
+
 
     </article>
 
