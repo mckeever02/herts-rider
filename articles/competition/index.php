f80c517c0efd3d2e91c2c34d5bb72e362c585413
Michael Mckeever <mckeever02@gmail.com>
2015-12-03 20:53:26 +0000

    Content added for Competition Page

--- articles/competition/index.php
+++ articles/competition/index.php
@@ -1,5 +1,5 @@
 <!-- Set the Page ID -->
-<?php $page_id = 13; ?>
+<?php $page_id = 7; ?>
 
 <?php include ("../../includes/variables.php"); ?>
 <?php include ("$path/includes/doctype.php"); ?>
@@ -29,104 +29,36 @@
 
     <article class="standard-article">
 
-        <section class="container wow fadeInUp">
+        <section class="container">
 
           <div class="longform-text-container">
-            <p class="introduction">Win the kit you need to stay safe on your bike, thanks to <a href="http://sportsbikeshop.co.uk" target="_blank">sportsbikeshop.co.uk</a>. You need the right kit when you’re on your motorbike, and thanks to SportsBikeShop you’ve got the chance to win over £750-worth of the best biking clobber.</p>
+            <div class="row">
+              <p class="introduction">Hertfordshire Rider has teamed up with Bikestop to give you the chance to win GoPro’s smallest action camera yet – the Hero4 Session.</p>
+            </div><!-- /row -->
 
-            <p>You don’t just need safety kit such as a helmet, jacket, boots, gloves and trousers, you also need security and protection. That’s why SportsBikeShop has included a chain lock worth £40 and a bike cover to keep your pride and joy sheltered from the elements. Just look at this lot...</p>
+            <p>Capable of capturing high-resolution 1440p30 and 1080p60 video, there’s even a 720p100 option that enables you to capture everything in pin-sharp slow motion.</p>
+
+            <div class="row">
+              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/gopro-image1.jpg" alt="Go Pro"></div>
+            </div><!-- /row -->
+
+          <p>Waterproof, shockproof and able to deliver still photos at the rate of 10 frames per second, the Hero4 Session can be controlled via just one button. With built-in wi-fi and Bluetooth, you can even control the Hero4 Session via an app on your smartphone.</p>
+
+          <p>Packing a pair of microphones and able to automatically deal with low light so you don’t have to think about it, the Hero4 Session is a miracle of technology in one tiny package. For the full SP, just log on to the <a href="https://shop.gopro.com/EMEA/cameras/hero4-session/CHDHS-101-EU.html">official GoPro page</a> to find out more about what the Hero4 Session can do.</p>
 
 
           </div><!-- /longform-text-container -->
 
-        </section>
 
-        <section class="container wow fadeInUp">
           <div class="row">
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/helmet.jpg" alt="Kabuto Kamui helmet"></div>
-              <div class="u-text-center">
-                <h2>Kabuto Kamui helmet</h2>
-                <h3>Worth £199.99</h3>
-              </div>
-            </div>
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/jacket.jpg" alt="ARMR Ashido 2 jacket"></div>
-              <div class="u-text-center">
-                <h2>ARMR Ashido 2 jacket</h2>
-                <h3>Worth £99.99</h3>
-              </div>
-            </div>
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/bike-cover.jpg" alt="Lampa Optima bike cover"></div>
-              <div class="u-text-center">
-                <h2>Lampa Optima bike cover</h2>
-                <h3>Worth £39.99</h3>
-              </div>
-            </div>
-          </row>
-        </section>
-        <section class="container">
-          <div class="row">
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/chain-lock.jpg" alt="Lampa Kiton chain lock"></div>
-              <div class="u-text-center">
-                <h2>Lampa Kiton chain lock</h2>
-                <h3>Worth £39.99</h3>
-              </div>
-            </div>
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/gloves.jpg" alt="ARMR S550 gloves"></div>
-              <div class="u-text-center">
-                <h2>ARMR S550 gloves</h2>
-                <h3>Worth £49.99</h3>
-              </div>
-            </div>
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:60%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/boots.jpg" alt="Falco Boots Oxegen"></div>
-              <div class="u-text-center">
-                <h2>Falco Boots Oxegen</h2>
-                <h3>Worth £129.99</h3>
-              </div>
-            </div>
-          </div><!--  /row -->
-        </section>
-        <section class="container">
-          <div class="row">
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/roadskin-jeans.jpg" alt="Roadskin Paranoid jeans"></div>
-              <div class="u-text-center">
-                <h2>Roadskin Paranoid jeans</h2>
-                <h3>Worth £119.99</h3>
-              </div>
-            </div>
-            <div class="four columns">
-              <div class="imgix-fluid-bg imgix-image" style="padding-bottom:100%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/indo-trousers.jpg" alt="ARMR Indo trousers"></div>
-              <div class="u-text-center">
-                <h2>ARMR Indo trousers</h2>
-                <h3>Worth £79.99</h3>
-              </div>
-            </div>
-          </div><!-- /row -->
-        </section>
+            <div class="imgix-fluid-bg imgix-image" style="padding-bottom:50%;" data-src="<? echo $imgix_path ?><? echo $page_issue2[''.$page_id.'']['id'];?>/images/gopro-image2.jpg" alt="Go Pro image 2"></div>
+          </div>
 
-
-          <div class="u-center-table">
-            <h2 class="u-text-center">Enter for your chance to win</h2>
-            <a href="https://www.surveymonkey.com/r/QZ5CFJM" class="button button-icon" style="display:block;" target="_blank">Click Here to Enter</a>
-
-<!--            <script>(function(t,e,c,s){var n,o,a;t.SMCX=t.SMCX||[],e.getElementById(s)||(n=e.getElementsByTagName(c),o=n[n.length-1],a=e.createElement(c),a.type="text/javascript",a.async=!0,a.id=s,a.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/SqaxiL9F5IL_2BaxSjltdpts7xBcz_2F81ztj34ctkSWg8M_2B39zZv0yh6FLfPUGKb_2B8u.js"].join(""),o.parentNode.insertBefore(a,o))})(window,document,"script","smcx-sdk");</script>-->
-          </div><!-- /u-text-center -->
-
-        </section>
-
-        <section class="container">
-          <div class="longform-text-container">
+          <div class="longform-text-container container-margin-top">
             <h4>The T's &amp; C's</h4>
             <ul>
-              <li>This competition is run by FirstCar Limited and SportsBikeShop Limited.</li>
-              <li>Entries must be recieved by midnight on 1 June 2016 after which the winner will be randomly selected</li>
-              <li>For full terms and conditions see <a href="http://firstbikemagazine.co.uk" target="_blank">www.firstbikemagazine.co.uk</a></li>
+              <li>This competition is run by FirstCar Limited and Bikestop Limited.</li>
+              <li>Entries must be received by midnight on 1 May 2016 after which the winner will be randomly selected.</li>
             </ul>
           </div>
 
